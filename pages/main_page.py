9cb29800494d99bed7553703037cd7bd8b6a91ab
jULL-ianna <162050566+jULL-ianna@users.noreply.github.com>
2025-05-16 19:21:15 +0300

    main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,17 +3,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class MainPage:
-    def __init__(self, driver):
-        self.driver = driver
-        self.url = "https://www.aviasales.ru"
-
+    def __init__(self, browser):
+        self.browser = browser
+    
     def open(self):
-        self.driver.get(self.url)
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='Откуда']"))
-        )
-
+        self.browser.get("https://www.aviasales.ru")
+    
     def search_flight(self, origin, destination):
-        self.driver.find_element(By.XPATH, "//input[@placeholder='Откуда']").send_keys(origin)
-        self.driver.find_element(By.XPATH, "//input[@placeholder='Куда']").send_keys(destination)
-        self.driver.find_element(By.XPATH, "//button[contains(text(), 'Найти билеты')]").click()
+        self.browser.find_element(By.XPATH, "//input[@placeholder='Откуда']").send_keys(origin)
+        self.browser.find_element(By.XPATH, "//input[@placeholder='Куда']").send_keys(destination)
+        self.browser.find_element(By.XPATH, "//button[contains(text(), 'Найти билеты')]").click()
+    
+    def type_origin_city(self, text):
+        self.browser.find_element(By.XPATH, "//input[@placeholder='Откуда']").send_keys(text)
+    
+    def is_search_button_enabled(self):
+        return self.browser.find_element(By.XPATH, "//button[contains(text(), 'Найти билеты')]").is_enabled()
+    
+    def open_date_picker(self):
+        self.browser.find_element(By.XPATH, "//div[contains(@class, 'trip-duration__date')]").click()
+    
+    def is_calendar_visible(self):
+        return self.browser.find_element(By.XPATH, "//div[contains(@class, 'calendar')]").is_displayed()
